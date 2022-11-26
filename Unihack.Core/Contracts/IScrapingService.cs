e53c680b37b310c7ddc43df61f5a283e37535285
Adelin Chis <adelin.chis@theaccessgroup.com>
2022-11-26 17:27:36 +0200

    Adding olx scrapping

---
+++ Unihack.Core/Contracts/IScrapingService.cs
@@ -0,0 +1,9 @@
+﻿using Unihack.Core.ViewModels;
+
+namespace Unihack.Core.Contracts
+{
+    public interface IScrapingService
+    {
+        Task<List<AdvertViewModel>> GetOlxPossibleAdverts(int stolenItemId);
+    }
+}
---
+++ Unihack.Core/ViewModels/AdvertViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Unihack.Core.ViewModels
+{
+    public class AdvertViewModel
+    {
+        public string AdvertUrl { get; set; }
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+    }
+}
